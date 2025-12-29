98aa684c8daa92fd9e1fa5516c8c4f55bf49a8f6
Lance Kian Flores <lancekian12@gmail.com>
2025-12-29 22:55:41 +0800

    added helper function for navigation

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path')
+const path = require("path");
+const rootDir = require("./utils/path");
 
 const app = express();
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,10 @@ const express = require("express");
 
 const router = express.Router();
 
+const rootDir = require('../utils/path')
+
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
