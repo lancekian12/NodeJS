428f12eff7ec7142492a7d036ee0fd188e6a99a8
Lance Kian Flores <lancekian12@gmail.com>
2025-12-29 22:21:51 +0800

    parsing the request

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const http = require("http");
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("middlware number 1");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
-  res.send("<h1>Hello from Product!</h1>");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
