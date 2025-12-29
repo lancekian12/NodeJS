b351c214874d6108ff66adfb6b23cdfc0358d292
Lance Kian Flores <lancekian12@gmail.com>
2025-12-30 00:45:23 +0800

    add other pug and a layout

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,8 @@ const rootDir = require("../utils/path");
 const products = []
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render('add-product', {pageTitle: 'Add Product'})
 });
 
 router.post("/add-product", (req, res, next) => {
