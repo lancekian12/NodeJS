5d89cb269573ea841e59c957e0daa76588368ab9
Lance Kian Flores <lancekian12@gmail.com>
2025-12-29 22:37:24 +0800

    adding route prefix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
