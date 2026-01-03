c1a5677144b19534c2c200ae3036968ebd5a03de
Lance Kian Flores <lancekian12@gmail.com>
2026-01-04 00:31:06 +0800

    linking to the editing page

--- models/product.js
+++ models/product.js
@@ -1,47 +1,48 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "product.json"
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-
-    if (!fileContent || fileContent.length === 0) {
-      return cb([]);
-    }
-    cb(JSON.parse(fileContent));
   });
 };
-module.exports = class Product {
-  constructor(tittle, imageUrl, description, price) {
-    this.title = tittle;
-    this.imageUrl = imageUrl
-    this.description = description
-    this.price = price
 
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
+
   save() {
     this.id = Math.random().toString();
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
   }
+
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
+
   static findById(id, cb) {
     getProductsFromFile(products => {
-      const product = products.find(p => p.id == id);
-      cb(product)
-    })
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
   }
 };
