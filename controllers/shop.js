67ba50b1472f529f54b191350c09767a1afd03c8
Lance Kian Flores <lancekian12@gmail.com>
2026-01-05 14:01:30 +0800

    getting a single product

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,9 +16,9 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findByPk(prodId)
-    .then(([product]) => {
+    .then((product) => {
       res.render("shop/product-detail", {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: "/products",
       });
