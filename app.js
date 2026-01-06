028c6395bbe38fc9d328a04b961c0510e023d5f5
Lance Kian Flores <lancekian12@gmail.com>
2026-01-07 04:55:33 +0800

    defining one to many and many to many relationship

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require("./controllers/error");
 const sequelize = require("./utils/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require('./models/cart')
+const CartItem = require('./models/cart-item')
 
 const app = express();
 
@@ -31,11 +33,15 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { contraints: true, onDelete: "CASCADE" });
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem})
+Product.belongsToMany(Cart, {through: CartItem});
 
 sequelize
-  .sync()
+  .sync({force: true})
   .then((result) => {
     return User.findByPk(1);
   })
