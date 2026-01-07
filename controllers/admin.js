d57d2e2ff37097cb774afb3366a8c8140c01921d
Lance Kian Flores <lancekian12@gmail.com>
2026-01-08 04:37:20 +0800

    Creating Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,99 +7,210 @@ exports.getAddProduct = async (req, res, next) => {
     editing: false,
   });
 };
-exports.postAddProduct = async (req, res, next) => {
-  try {
-    const { title, imageUrl, price, description } = req.body;
-    await req.user.createProduct({
-      title: title,
-      price: price,
-      imageUrl: imageUrl,
-      description: description,
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, price, description, imageUrl);
+  product
+    .save()
+    .then(result => {
+      // console.log(result);
+      console.log('Created Product');
+      res.redirect('/admin/products');
+    })
+    .catch(err => {
+      console.log(err);
     });
-    console.log("Created Product");
-    res.redirect("/admin/produc ts");
-  } catch (err) {
-    console.log(err);
-  }
 };
 
-exports.getEditProduct = async (req, res, next) => {
-  try {
-    const editMode = req.query.edit;
-    if (!editMode) {
-      return res.redirect("/");
-    }
+// exports.getEditProduct = async (req, res, next) => {
+//   try {
+//     const editMode = req.query.edit;
+//     if (!editMode) {
+//       return res.redirect("/");
+//     }
 
-    const prodId = req.params.productId;
-    const product = await req.user.getProducts({where: {id: prodId}})
+//     const prodId = req.params.productId;
+//     const product = await req.user.getProducts({where: {id: prodId}})
  
-    if (!product) {
-      return res.redirect("/");
-    }
+//     if (!product) {
+//       return res.redirect("/");
+//     }
 
-    res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
-      editing: editMode,
-      product,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
+//     res.render("admin/edit-product", {
+//       pageTitle: "Edit Product",
+//       path: "/admin/edit-product",
+//       editing: editMode,
+//       product,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-exports.postEditProduct = async (req, res, next) => {
-  try {
-    const { productId, title, price, imageUrl, description } = req.body;
+// exports.postEditProduct = async (req, res, next) => {
+//   try {
+//     const { productId, title, price, imageUrl, description } = req.body;
 
-    const product = await Product.findByPk(productId);
+//     const product = await Product.findByPk(productId);
 
-    if (!product) {
-      return res.redirect("/");
-    }
+//     if (!product) {
+//       return res.redirect("/");
+//     }
 
-    product.title = title;
-    product.price = price;
-    product.imageUrl = imageUrl;
-    product.description = description;
+//     product.title = title;
+//     product.price = price;
+//     product.imageUrl = imageUrl;
+//     product.description = description;
 
-    await product.save();
+//     await product.save();
 
-    console.log("UPDATED PRODUCT!");
-    res.redirect("/admin/products");
-  } catch (err) {
-    console.log(err);
-  }
-};
+//     console.log("UPDATED PRODUCT!");
+//     res.redirect("/admin/products");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-exports.getProducts = async (req, res, next) => {
-  try {
-    const products = await req.user.getProducts()
+// exports.getProducts = async (req, res, next) => {
+//   try {
+//     const products = await req.user.getProducts()
 
-    res.render("admin/products", {
-      prods: products,
-      pageTitle: "Admin Products",
-      path: "/admin/products",
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
+//     res.render("admin/products", {
+//       prods: products,
+//       pageTitle: "Admin Products",
+//       path: "/admin/products",
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-exports.postDeleteProduct = async (req, res, next) => {
-  try {
-    const prodId = req.body.productId;
-    const product = await Product.findByPk(prodId);
+// exports.postDeleteProduct = async (req, res, next) => {
+//   try {
+//     const prodId = req.body.productId;
+//     const product = await Product.findByPk(prodId);
 
-    if (!product) {
-      return res.redirect("/admin/products");
-    }
+//     if (!product) {
+//       return res.redirect("/admin/products");
+//     }
 
-    await product.destroy();
+//     await product.destroy();
 
-    console.log("DESTROYED PRODUCT");
-    res.redirect("/admin/products");
-  } catch (err) {
-    console.log(err);
-  }
-};
+//     console.log("DESTROYED PRODUCT");
+//     res.redirect("/admin/products");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+
+/////////////////////////////////////////////////// SQL
+// const Product = require("../models/product");
+
+// exports.getAddProduct = async (req, res, next) => {
+//   res.render("admin/edit-product", {
+//     pageTitle: "Add Product",
+//     path: "/admin/add-product",
+//     editing: false,
+//   });
+// };
+// exports.postAddProduct = async (req, res, next) => {
+//   try {
+//     const { title, imageUrl, price, description } = req.body;
+//     await req.user.createProduct({
+//       title: title,
+//       price: price,
+//       imageUrl: imageUrl,
+//       description: description,
+//     });
+//     console.log("Created Product");
+//     res.redirect("/admin/produc ts");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// exports.getEditProduct = async (req, res, next) => {
+//   try {
+//     const editMode = req.query.edit;
+//     if (!editMode) {
+//       return res.redirect("/");
+//     }
+
+//     const prodId = req.params.productId;
+//     const product = await req.user.getProducts({where: {id: prodId}})
+ 
+//     if (!product) {
+//       return res.redirect("/");
+//     }
+
+//     res.render("admin/edit-product", {
+//       pageTitle: "Edit Product",
+//       path: "/admin/edit-product",
+//       editing: editMode,
+//       product,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// exports.postEditProduct = async (req, res, next) => {
+//   try {
+//     const { productId, title, price, imageUrl, description } = req.body;
+
+//     const product = await Product.findByPk(productId);
+
+//     if (!product) {
+//       return res.redirect("/");
+//     }
+
+//     product.title = title;
+//     product.price = price;
+//     product.imageUrl = imageUrl;
+//     product.description = description;
+
+//     await product.save();
+
+//     console.log("UPDATED PRODUCT!");
+//     res.redirect("/admin/products");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// exports.getProducts = async (req, res, next) => {
+//   try {
+//     const products = await req.user.getProducts()
+
+//     res.render("admin/products", {
+//       prods: products,
+//       pageTitle: "Admin Products",
+//       path: "/admin/products",
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// exports.postDeleteProduct = async (req, res, next) => {
+//   try {
+//     const prodId = req.body.productId;
+//     const product = await Product.findByPk(prodId);
+
+//     if (!product) {
+//       return res.redirect("/admin/products");
+//     }
+
+//     await product.destroy();
+
+//     console.log("DESTROYED PRODUCT");
+//     res.redirect("/admin/products");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
--- controllers/shop.js
+++ controllers/shop.js
@@ -154,3 +154,162 @@ exports.getCheckout = async (req, res, next) => {
     pageTitle: "Checkout",
   });
 };
+
+
+/////////////////////////////////////////// SQL
+// const Product = require("../models/product");
+// const Cart = require("../models/cart");
+
+// exports.getProducts = async (req, res, next) => {
+//   try {
+//     const products = await Product.findAll();
+//     res.render("shop/product-list", {
+//       prods: products,
+//       pageTitle: "All Products",
+//       path: "/products",
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// exports.getProduct = async (req, res, next) => {
+//   try {
+//     const prodId = req.params.productId;
+//     const product = await Product.findByPk(prodId);
+
+//     res.render("shop/product-detail", {
+//       product: product,
+//       pageTitle: product.title,
+//       path: "/products",
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// exports.getIndex = async (req, res, next) => {
+//   try {
+//     const products = await Product.findAll();
+//     res.render("shop/index", {
+//       prods: products,
+//       pageTitle: "Shop",
+//       path: "/",
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// exports.getCart = async (req, res, next) => {
+//   try {
+//     const cart = await req.user.getCart();
+//     const products = await cart.getProducts();
+
+//     res.render("shop/cart", {
+//       path: "/cart",
+//       pageTitle: "Your Cart",
+//       products: products,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// exports.postCart = async (req, res, next) => {
+//   try {
+//     const prodId = req.body.productId;
+//     let newQuantity = 1;
+
+//     const cart = await req.user.getCart();
+//     const products = await cart.getProducts({ where: { id: prodId } });
+
+//     let product;
+//     if (products.length > 0) {
+//       product = products[0];
+//       newQuantity = product.cartItem.quantity + 1;
+//     } else {
+//       product = await Product.findByPk(prodId);
+//     }
+
+//     await cart.addProduct(product, {
+//       through: { quantity: newQuantity },
+//     });
+
+//     res.redirect("/cart");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// // req.user.getCart().then((cart) => {
+// //   return cart.getProducts({ where: { id: prodId } });
+// // }).then(products => {
+// //   const product = products[0];
+// //   return product.cartItem.destroy();
+// // }).then(result => {
+// //   res.redirect('/cart')
+// // })
+
+// exports.postCartDeleteProduct = async (req, res, next) => {
+//   try {
+//     const prodId = req.body.productId;
+//     const cart = await req.user.getCart();
+//     const products = await cart.getProducts({ where: { id: prodId } });
+//     if (!products.length) {
+//       return res.redirect("/cart");
+//     }
+
+//     await products[0].cartItem.destroy();
+//     res.redirect("/cart");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// exports.postOrder = async (req, res, next) => {
+//   try {
+//     const cart = await req.user.getCart();
+//     const products = await cart.getProducts();
+
+//     const order = await req.user.createOrder();
+
+//     await order.addProducts(
+//       products.map(product => {
+//         product.orderItem = {
+//           quantity: product.cartItem.quantity
+//         };
+//         return product;
+//       })
+//     );
+
+//     await cart.setProducts(null);
+//     res.redirect('/orders');
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+// exports.getOrders = async (req, res, next) => {
+//   try {
+//     const orders = await req.user.getOrders({
+//       include: ['products']
+//     });
+
+//     res.render('shop/orders', {
+//       path: '/orders',
+//       pageTitle: 'Your Orders',
+//       orders
+//     });
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+
+// exports.getCheckout = async (req, res, next) => {
+//   res.render("shop/checkout", {
+//     path: "/checkout",
+//     pageTitle: "Checkout",
+//   });
+// };
