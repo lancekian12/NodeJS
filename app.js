1442c95f1aed89e18896f6e0ab103b3cdf2ebd83
Lance Kian Flores <lancekian12@gmail.com>
2026-01-08 07:04:51 +0800

    creating new user in nosql

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const { mongoConnect } = require("./utils/database");
+const User = require('./models/user')
 
 const app = express();
 
@@ -20,12 +21,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
+  User.findById('695ee61f05744e0a5b9f0433')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
   next();
 });
 
