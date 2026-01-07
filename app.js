0175f6c9c9c5f1284b2adc132ebd421920beb63e
Lance Kian Flores <lancekian12@gmail.com>
2026-01-08 04:12:12 +0800

    connect mongodb

--- app.js
+++ app.js
@@ -1,69 +1,109 @@
+require("dotenv").config();
+
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
-const rootDir = require("./utils/path");
 
 const errorController = require("./controllers/error");
-const sequelize = require("./utils/database");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+const mongoConnect = require("./utils/database");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
+  // User.findById(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
+mongoConnect((client) => {
+  app.listen(3000);
+});
 
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "lance kian", email: "test@gmail.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    console.log(user);
-    return user.createCart();
-  })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((err) => console.log(err));
+////////////////////////////////////////////// SEQUELIZE DATABASE
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const path = require("path");
+// const rootDir = require("./utils/path");
+
+// const errorController = require("./controllers/error");
+// const sequelize = require("./utils/database");
+// const Product = require("./models/product");
+// const User = require("./models/user");
+// const Cart = require("./models/cart");
+// const CartItem = require("./models/cart-item");
+// const Order = require("./models/order");
+// const OrderItem = require("./models/order-item");
+
+// const app = express();
+
+// app.set("view engine", "ejs");
+// app.set("views", "views");
+
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.use((req, res, next) => {
+//   User.findByPk(1)
+//     .then((user) => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
+
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
+
+// app.use(errorController.get404);
+
+// Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+// User.hasMany(Product);
+// User.hasOne(Cart);
+// Cart.belongsTo(User);
+// Cart.belongsToMany(Product, { through: CartItem });
+// Product.belongsToMany(Cart, { through: CartItem });
+// Order.belongsTo(User);
+// User.hasMany(Order);
+// Order.belongsToMany(Product, { through: OrderItem });
+
+// sequelize
+//   // .sync({ force: true })
+//   .sync()
+//   .then((result) => {
+//     return User.findByPk(1);
+//   })
+//   .then((user) => {
+//     if (!user) {
+//       return User.create({ name: "lance kian", email: "test@gmail.com" });
+//     }
+//     return user;
+//   })
+//   .then((user) => {
+//     console.log(user);
+//     return user.createCart();
+//   })
+//   .then((cart) => {
+//     app.listen(3000);
+//   })
+//   .catch((err) => console.log(err));
--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,35 @@
+const { MongoClient } = require('mongodb');
 
-const Sequelize = require('sequelize')
-require('dotenv').config()
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {dialect: 'mysql', host: process.env.DB_HOST})
+const mongoConnect = async (callback) => {
+  try {
+    const user = process.env.DB_USER || 'lancekian';
+    const password = process.env.DB_PASSWORD;
+    const host = process.env.DB_HOST || 'nodejs-course.0ggknfc.mongodb.net';
+    const dbName = process.env.DB_NAME || 'myDatabase';
 
+    if (!password) {
+      throw new Error('Missing DB_PASSWORD in .env');
+    }
 
-module.exports = sequelize
+    // If your password contains characters that need encoding, encode it:
+    const encodedPassword = encodeURIComponent(password);
+
+    const uri = process.env.MONGODB_URI || 
+      `mongodb+srv://${user}:${encodedPassword}@${host}/${dbName}?retryWrites=true&w=majority`;
+
+    const client = await MongoClient.connect(uri);
+    console.log('Connected to MongoDB!');
+    callback(client);
+  } catch (err) {
+    console.error('MongoDB connection failed:', err);
+    throw err;
+  }
+};
+
+module.exports = mongoConnect;
+
+// const Sequelize = require('sequelize')
+// require('dotenv').config()
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {dialect: 'mysql', host: process.env.DB_HOST})
+
+// module.exports = sequelize
