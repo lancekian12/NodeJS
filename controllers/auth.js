57d9c352c9a8b0ee826e48e78059b31b3f6a2277
Lance Kian Flores <lancekian12@gmail.com>
2026-01-10 15:05:50 +0800

    deleting a session and a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,3 +10,9 @@ exports.postLogin = (req, res, next) => {
   req.session.isLoggedIn = true;
   res.redirect("/");
 };
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(() => {
+    res.redirect('/')
+  })
+};
