e9d160ca52c49fe5f2fd3dfb0424b981de86063c
Lance Kian Flores <lancekian12@gmail.com>
2026-01-13 11:38:17 +0800

    validating password

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,51 @@
-const express = require('express');
-const { check } = require('express-validator');
+const express = require("express");
+const { check, body } = require("express-validator");
 
-const authController = require('../controllers/auth');
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
+router.get("/login", authController.getLogin);
 
-router.get('/signup', authController.getSignup);
+router.get("/signup", authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post("/login", authController.postLogin);
 
-router.post('/signup', check('email').isEmail().withMessage('Please Enter a Valid Email'), authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        if (value === "test@test.com") {
+          throw new Error("This email address if forbidden.");
+        }
+        return true;
+      }),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters."
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords have to match!");
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup
+);
+router.post("/logout", authController.postLogout);
 
-router.post('/logout', authController.postLogout);
+router.get("/reset", authController.getReset);
 
-router.get('/reset', authController.getReset);
+router.post("/reset", authController.postReset);
 
-router.post('/reset', authController.postReset);
+router.get("/reset/:token", authController.getNewPassword);
 
-router.get('/reset/:token', authController.getNewPassword);
-
-router.post('/new-password', authController.postNewPassword);
+router.post("/new-password", authController.postNewPassword);
 
 module.exports = router;
