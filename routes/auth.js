383d6009afcc0d7d15cf9e07351d2d7874cd600c
Lance Kian Flores <lancekian12@gmail.com>
2026-01-13 12:00:58 +0800

    sanitizing data

--- routes/auth.js
+++ routes/auth.js
@@ -14,47 +14,55 @@ router.post(
   [
     body('email')
       .isEmail()
-      .withMessage('Please enter a valid email address.'),
+      .withMessage('Please enter a valid email address.')
+      .normalizeEmail(),
     body('password', 'Password has to be valid.')
       .isLength({ min: 5 })
       .isAlphanumeric()
+      .trim()
   ],
   authController.postLogin
 );
+
 router.post(
-  "/signup",
+  '/signup',
   [
-    check("email")
+    check('email')
       .isEmail()
-      .withMessage("Please enter a valid email.")
+      .withMessage('Please enter a valid email.')
       .custom((value, { req }) => {
         // if (value === 'test@test.com') {
         //   throw new Error('This email address if forbidden.');
         // }
         // return true;
-        return User.findOne({ email: value }).then((userDoc) => {
+        return User.findOne({ email: value }).then(userDoc => {
           if (userDoc) {
             return Promise.reject(
-              "E-Mail exists already, please pick a different one."
+              'E-Mail exists already, please pick a different one.'
             );
           }
         });
-      }),
+      })
+      .normalizeEmail(),
     body(
-      "password",
-      "Please enter a password with only numbers and text and at least 5 characters."
+      'password',
+      'Please enter a password with only numbers and text and at least 5 characters.'
     )
       .isLength({ min: 5 })
-      .isAlphanumeric(),
-    body("confirmPassword").custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error("Passwords have to match!");
-      }
-      return true;
-    }),
+      .isAlphanumeric()
+      .trim(),
+    body('confirmPassword')
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!');
+        }
+        return true;
+      })
   ],
   authController.postSignup
 );
+
 
 router.post("/logout", authController.postLogout);
 
