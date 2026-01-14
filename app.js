e715f87da49c229b07f889c66e98a11b5d2270f1
Lance Kian Flores <lancekian12@gmail.com>
2026-01-14 18:37:17 +0800

    filter valid file only

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const store = new MongoDBStore({
 });
 
 const csrfProtection = csrf();
+
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "images");
@@ -34,6 +35,17 @@ const fileStorage = multer.diskStorage({
     cb(null, new Date().toISOString() + "-" + file.originalname);
   },
 });
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -44,8 +56,9 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(multer({ storage: fileStorage }).single("image"));
-
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
