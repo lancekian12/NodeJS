0482afd38391a17a19598b55a7a65bf78903023b
Lance Kian Flores <lancekian12@gmail.com>
2026-01-20 21:04:25 +0800

    feat: generating pdf with order data

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,6 +4,8 @@ const path = require("path");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
+const PDFDocument = require('pdfkit')
+
 exports.getProducts = async (req, res, next) => {
   try {
     const products = await Product.find();
@@ -165,12 +167,34 @@ exports.getInvoice = async (req, res, next) => {
     const invoiceName = `invoice-${orderId}.pdf`;
     const invoicePath = path.join("data", "invoices", invoiceName);
 
-    const file = fs.createReadStream(invoicePath);
+    const pdfDoc = new PDFDocument();
 
     res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Content-Disposition", `inline; filename="${invoiceName}"`);
+    res.setHeader(
+      "Content-Disposition",
+      `inline; filename="${invoiceName}"`
+    );
 
-    file.pipe(res);
+    pdfDoc.pipe(fs.createWriteStream(invoicePath));
+    pdfDoc.pipe(res);
+
+    pdfDoc.fontSize(26).text("Invoice", { underline: true });
+    pdfDoc.text("-----------------------");
+
+    let totalPrice = 0;
+    order.products.forEach(prod => {
+      totalPrice += prod.quantity * prod.product.price;
+      pdfDoc
+        .fontSize(14)
+        .text(
+          `${prod.product.title} - ${prod.quantity} x $${prod.product.price}`
+        );
+    });
+
+    pdfDoc.text("---");
+    pdfDoc.fontSize(20).text(`Total Price: $${totalPrice}`);
+
+    pdfDoc.end();
   } catch (err) {
     next(err);
   }
