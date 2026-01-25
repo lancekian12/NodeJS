d31d6dae16bf1618dbe4213188d927acc6b5dc03
Lance Kian Flores <lancekian12@gmail.com>
2026-01-26 00:30:07 +0800

    feat: handling static images and error handling

--- nodejs-rest/backend/controllers/feed.js
+++ nodejs-rest/backend/controllers/feed.js
@@ -19,28 +19,30 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: "Validation failed, entered data is incorrect.",
-      errors: errors.array(),
-    });
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: "images/reactangle.png",
-    creator: { name: "Lance Kian Flores" },
+    imageUrl: 'images/Rectangle.png',
+    creator: { name: 'Lance Kian Flores' }
   });
   post
     .save()
-    .then((result) => {
+    .then(result => {
       res.status(201).json({
-        message: "Post created successfully!",
-        post: result,
+        message: 'Post created successfully!',
+        post: result
       });
     })
-    .catch((err) => {
-      console.log(err);
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
     });
 };
