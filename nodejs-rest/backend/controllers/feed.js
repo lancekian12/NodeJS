24e17ff4309c985a5e5ffa5b48c91551422eace3
Lance Kian Flores <lancekian12@gmail.com>
2026-01-26 00:27:41 +0800

    feat: storing post in database

--- nodejs-rest/backend/controllers/feed.js
+++ nodejs-rest/backend/controllers/feed.js
@@ -1,5 +1,5 @@
-const { validationResult } = require('express-validator');
-
+const { validationResult } = require("express-validator");
+const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
@@ -20,8 +20,8 @@ exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({
-      message: 'Validation failed, entered data is incorrect.',
-      errors: errors.array()
+      message: "Validation failed, entered data is incorrect.",
+      errors: errors.array(),
     });
   }
   const title = req.body.title;
@@ -29,18 +29,18 @@ exports.createPost = (req, res, next) => {
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/duck.jpg',
-    creator: { name: 'Maximilian' }
+    imageUrl: "images/reactangle.png",
+    creator: { name: "Lance Kian Flores" },
   });
   post
     .save()
-    .then(result => {
+    .then((result) => {
       res.status(201).json({
-        message: 'Post created successfully!',
-        post: result
+        message: "Post created successfully!",
+        post: result,
       });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
     });
 };
