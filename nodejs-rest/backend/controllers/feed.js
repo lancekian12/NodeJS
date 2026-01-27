cbf619f4d80dd5780df3c38908abbb5b14b9fe6d
Lance Kian Flores <lancekian12@gmail.com>
2026-01-28 05:23:55 +0800

    feat: clearing post user relations

--- nodejs-rest/backend/controllers/feed.js
+++ nodejs-rest/backend/controllers/feed.js
@@ -152,27 +152,40 @@ exports.updatePost = (req, res, next) => {
 exports.deletePost = (req, res, next) => {
   const postId = req.params.postId;
   Post.findById(postId)
-    .then((post) => {
+    .then(post => {
       if (!post) {
-        const error = new Error("Could not find post.");
+        const error = new Error('Could not find post.');
         error.statusCode = 404;
+        throw error;
+      }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized!');
+        error.statusCode = 403;
         throw error;
       }
       // Check logged in user
       clearImage(post.imageUrl);
-      return Post.findByIdAndDelete(postId);
+      return Post.findByIdAndRemove(postId);
     })
-    .then((result) => {
-      console.log(result);
-      res.status(200).json({ message: "Deleted post." });
+    .then(result => {
+      return User.findById(req.userId);
     })
-    .catch((err) => {
+    .then(user => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then(result => {
+      res.status(200).json({ message: 'Deleted post.' });
+    })
+    .catch(err => {
       if (!err.statusCode) {
         err.statusCode = 500;
       }
       next(err);
     });
 };
+
+
 
 const clearImage = (filePath) => {
   filePath = path.join(__dirname, "..", filePath);
