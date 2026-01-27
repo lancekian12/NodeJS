ab82755edd0a40462a1a8e2380409d676a11f096
Lance Kian Flores <lancekian12@gmail.com>
2026-01-28 05:22:42 +0800

    feat: adding authorization checks

--- nodejs-rest/backend/controllers/feed.js
+++ nodejs-rest/backend/controllers/feed.js
@@ -101,45 +101,53 @@ exports.getPost = (req, res, next) => {
 exports.updatePost = (req, res, next) => {
   const postId = req.params.postId;
   const errors = validationResult(req);
-
   if (!errors.isEmpty()) {
-    const error = new Error("Validation failed, entered data is incorrect.");
+    const error = new Error('Validation failed, entered data is incorrect.');
     error.statusCode = 422;
     throw error;
   }
-
   const title = req.body.title;
   const content = req.body.content;
-
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+  if (!imageUrl) {
+    const error = new Error('No file picked.');
+    error.statusCode = 422;
+    throw error;
+  }
   Post.findById(postId)
-    .then((post) => {
+    .then(post => {
       if (!post) {
-        const error = new Error("Could not find post.");
+        const error = new Error('Could not find post.');
         error.statusCode = 404;
         throw error;
       }
-
-      let imageUrl = post.imageUrl;
-
-      if (req.file) {
-        imageUrl = req.file.path.replace(/\\/g, "/");
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized!');
+        error.statusCode = 403;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
-
       post.title = title;
-      post.content = content;
       post.imageUrl = imageUrl;
-
+      post.content = content;
       return post.save();
     })
-    .then((result) => {
-      res.status(200).json({ message: "Post updated!", post: result });
+    .then(result => {
+      res.status(200).json({ message: 'Post updated!', post: result });
     })
-    .catch((err) => {
-      if (!err.statusCode) err.statusCode = 500;
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
       next(err);
     });
 };
+
 
 exports.deletePost = (req, res, next) => {
   const postId = req.params.postId;
