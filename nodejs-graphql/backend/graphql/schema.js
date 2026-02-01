149bd496bbdf288d574b33c3417674ac931c892e
Lance Kian Flores <lancekian12@gmail.com>
2026-02-01 11:27:22 +0800

    feat: create mutation resolver and play around graphiql

--- nodejs-graphql/backend/graphql/schema.js
+++ nodejs-graphql/backend/graphql/schema.js
@@ -10,24 +10,32 @@ module.exports = buildSchema(`
         createdAt: String!
         updatedAt: String!
     }
+
     type User {
         _id: ID!
         name: String!
         email: String!
         password: String
         status: String!
-        post: [Post!]!
+        posts: [Post!]!
     }
+
     input UserInputData {
         email: String!
         name: String!
         password: String!
     }
-    type RootMutation {
-        createUser(userInput: UserInputData)
+
+    type RootQuery {
+        hello: String
     }
+
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+    }
+
     schema {
         query: RootQuery
+        mutation: RootMutation
     }
-    
 `);
